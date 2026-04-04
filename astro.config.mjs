4402b9a0a9a0653aa854eeb268bfdeccaddd42a1
Kyoojin-san <pedro@somaconcursos.com.br>
2026-04-03 23:42:50 -0300

    feat: sitemap automatico, robots.txt e remocao de travessoes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 import tailwindcss from "@tailwindcss/vite"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ruche.com.br",
   output: "static",
+  integrations: [react(), sitemap()],
   compressHTML: true,
-  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
